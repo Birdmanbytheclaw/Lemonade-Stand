2b69fd95975a8d55b66e791ec99a8d600c6a13e1
Birdmanbytheclaw <therealparkerflynn@gmail.com>
2020-01-16 15:41:31 -0600

    Got weather almost set

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -11,6 +11,13 @@ namespace LemonadeStand
         private List<string> weatherConditions;
         public string predictedForecast;
 
+        public Weather()
+        {
+            weatherConditions = new List<string>() { "Sunny", "Cloudy", "Foggy", "Windy", "Rainy" };
+
+        }
+
+
         public int GetTempurature(Random Rnd)
         {
             int temperature = Rnd.Next(1, 5);
@@ -21,13 +28,13 @@ namespace LemonadeStand
                     this.temperature = 90;
                     break;
                 case 2:
-                    this.temperature = 70;
+                    this.temperature = 80;
                     break;
                 case 3:
-                    this.temperature = 50;
+                    this.temperature = 70;
                     break;
                 case 4:
-                    this.temperature = 30;
+                    this.temperature = 60;
                     break;
                 default:
                     Console.WriteLine("Crazy weather, Cannot find report");
@@ -35,8 +42,6 @@ namespace LemonadeStand
             }
             return temperature;
         }
-        List<>
-      
-        
+            
     }
 }
