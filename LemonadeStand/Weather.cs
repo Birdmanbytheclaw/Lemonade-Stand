902f3d7cc168d16a9c0919e5a68c25ee4386b7b5
Birdmanbytheclaw <therealparkerflynn@gmail.com>
2020-01-17 13:51:06 -0600

    forecast figured

--- LemonadeStand/Weather.cs
+++ LemonadeStand/Weather.cs
@@ -10,18 +10,19 @@ namespace LemonadeStand
         public int temperature;
         private List<string> weatherConditions;
         public List<string> Forecast;
+        public string forecast;
 
         public Weather()
         {
             weatherConditions = new List<string>() { "Sunny", "Cloudy", "Foggy", "Windy", "Rainy" };
-
+            Forecast = new List<string>() {"Hot", "Cold", "Warm", "Cool" };
         }
 
         public void ChooseCondition()
         {
             Random PickCondition = new Random();
             condition = weatherConditions[PickCondition.Next(1,5)];
-            Console.WriteLine(condition);
+            Console.WriteLine("and " + condition);
         }
 
         public int GetTempurature(Random Rnd)
@@ -29,10 +30,17 @@ namespace LemonadeStand
             temperature = Rnd.Next(60, 90);
             return temperature;
         }
-     /* public string PredictedForecast()
+
+        public void ChooseForecast()
         {
-            Forecast = new List<string>() { };
-        }*/
-            
+            Random PickForecast = new Random();
+            forecast = Forecast[PickForecast.Next(1, 4)];
+            Console.WriteLine(forecast);
+        }
+        public void PredictedForecast()
+        {
+            Console.WriteLine("It will be" + forecast + " and partly" + condition);
+        }
+
     }
 }
